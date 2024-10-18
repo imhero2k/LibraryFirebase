11676deac20e083a6a4b49a640a44a1366f73f32
saran <skan0077@student.monash.edu>
2024-10-18 18:41:14 +1100

    addding cors to vite

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    cors: false, // Disable CORS in the dev server
+    proxy: {
+      '/weather': {
+        target: 'https://api.openweathermap.org/', // The OpenWeather API endpoint
+        changeOrigin: true, // Change the origin of the host header to the target URL
+        secure: true, // Ensure SSL verification is enabled
+        rewrite: (path) => path.replace(/^\/weather/, ''), // Remove '/weather' from the path
+      },
+    }
+  },
+  
   plugins: [
     vue(),
     vueDevTools(),
